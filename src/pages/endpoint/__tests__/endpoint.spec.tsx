7ba4369f94bb5ee6859319a86da7f40305982d28
Daniel Andeche <139417311+Danielandeche@users.noreply.github.com>
2024-12-24 03:05:40 +0000

    web socket

--- src/pages/endpoint/__tests__/endpoint.spec.tsx
+++ src/pages/endpoint/__tests__/endpoint.spec.tsx
@@ -48,7 +48,7 @@ describe('<Endpoint />', () => {
         await userEvent.type(appIdInput, '123');
         await userEvent.click(resetButton);
 
-        expect(LocalStorageUtils.getValue('config.server_url') ?? '').toBe('blue.derivws.com');
-        expect(LocalStorageUtils.getValue('config.app_id') ?? '').toBe(65555);
+        expect(LocalStorageUtils.getValue('config.server_url') ?? '').toBe('ws.derivws.com');
+        expect(LocalStorageUtils.getValue('config.app_id') ?? '').toBe(66622);
     });
 });
